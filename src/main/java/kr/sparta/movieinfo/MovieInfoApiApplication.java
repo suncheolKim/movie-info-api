c96b99a775043b0afeae6c03c3d2d287ca1b44ce
Suncheol Kim <suncheol.kim84@gmail.com>
2024-06-13 21:29:33 +0900

    최초 커밋

---
+++ src/main/java/kr/sparta/movieinfo/MovieInfoApiApplication.java
@@ -0,0 +1,13 @@
+package kr.sparta.movieinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieInfoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieInfoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/sparta/movieinfo/MovieInfoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.sparta.movieinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
