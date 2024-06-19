93b7b8b8d47d16c064e95a59977fafbf874f465c
Suncheol Kim <suncheol.kim84@gmail.com>
2024-06-19 18:25:24 +0900

    feat : 변경사항을 눈으로 확인하기 위해 API 호출 마지막에 현재시간 출력

--- src/main/java/kr/sparta/movieinfo/info/MovieInfoService.java
+++ src/main/java/kr/sparta/movieinfo/info/MovieInfoService.java
@@ -2,10 +2,13 @@ package kr.sparta.movieinfo.info;
 
 import kr.sparta.movieinfo.info.dto.MovieInfoDto;
 import kr.sparta.movieinfo.info.model.MovieInfo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
+@Slf4j
 @Service
 public class MovieInfoService {
     private final MovieInfoRepository movieInfoRepository;
@@ -16,15 +19,17 @@ public class MovieInfoService {
 
     public List<MovieInfoDto> getMovieInfoList() {
         final List<MovieInfo> movies = movieInfoRepository.findAll();
-
+        log.info(LocalDateTime.now().toString());
         return movies.stream()
                 .map(MovieInfoDto::new)
                 .toList();
     }
 
     public MovieInfoDto getMovieInfo(Long movieNo) {
-        return movieInfoRepository.findById(movieNo)
+        MovieInfoDto movieInfoDto = movieInfoRepository.findById(movieNo)
                 .map(MovieInfoDto::new)
                 .orElseThrow(() -> new IllegalArgumentException("No data found {movieNo: " + movieNo + "}"));
+        log.info(LocalDateTime.now().toString());
+        return movieInfoDto;
     }
 }
